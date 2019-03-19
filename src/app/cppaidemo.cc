43f6bf1f1f731b0a76b2a705bd1ecceca91bb52c
jptboy <vegetablebat1@gmail.com>
2019-03-19 01:53:12 -0600

    probably done

--- src/app/cppaidemo.cc
+++ src/app/cppaidemo.cc
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <Vec.hpp>
 #include <boost/range/irange.hpp>
+#include <Matrix.hpp>
 
 int main(int argc, char const *argv[])
 {
+    using uint = unsigned int;
     if(argc < 2)
     {
         std::cerr << "Invalid number of arguments\n";
-        std::cerr << "Usage: NNdemo integer\n";
+        std::cerr << "Usage: cppaidemo integer\n";
         return -1;
     }
     auto rng = std::stoi(argv[1]);
@@ -22,6 +24,14 @@ int main(int argc, char const *argv[])
     {
         std::cout << myVec[i] << "\n";
     }
+
+
+    const uint n = 3;
+    const uint m = 3;
+    uint* const* const matrix = (uint* const* const)(new uint[n][m]);
+    stdAyush::Matrix matr(n,m,matrix);
+
+
     return 0;
 }
 
